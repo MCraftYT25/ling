2df8b453f01ce1823adb623e9aafd96b01fc4a22
MCraftYT25 <93701162+MCraftYT25@users.noreply.github.com>
2023-10-14 13:19:33 +0200

    Add files via upload
    V0.5

--- JS/main-script.js
+++ JS/main-script.js
@@ -1,4 +1,4 @@
-const VERSION = "0.3";
+const VERSION = "0.5";
 const VERSIONDATE = "2023-10-14";
 const YEAR = "2023";
 const COMPANY = "VSK1 GmbH";
@@ -10,4 +10,14 @@ function main()
     document.getElementById('footer').innerText = COMPANY + " " + YEAR + " " + "V" + VERSION;
 }
 
-
+let menu_value = 1;
+function menu()
+{
+    menu_value++;
+    if(menu_value % 2 == 0)
+    {
+        document.getElementById('menu-list-cont').innerHTML = '<div id="menu-list"><a>Zaloguj</a><a>Załóż konto</a><a>Settings</a></div>';
+    } else {
+        document.getElementById('menu-list-cont').innerText = '';
+    } 
+}
